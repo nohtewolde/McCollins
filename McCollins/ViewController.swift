fc82ecbfc30d1770f32634adfe0b27d7a97cebaa
Noh Tewolde <nohtewolde@nohs-mbp.attlocal.net>
2019-03-28 21:45:45 -0500

    Initial Commit

---
+++ McCollins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  McCollins
+//
+//  Created by Noh Tewolde on 3/28/19.
+//  Copyright © 2019 Noh Tewolde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
